25912a0792431dd47c032cbd37581fc51474d382
yaginuuu <yaginuuu56@gmail.com>
2015-06-21 15:42:54 +0900

    とりあえずデプロイ

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
     location_id = 284860
-    @medias = Instagram.tag_recent_media(URI.encode("笑顔")) +
+    @medias = Instagram.tag_recent_media(URI.encode("笑顔")) &&
+              Instagram.location_recent_media(location_id) ||
+              Instagram.tag_recent_media(URI.encode("元気")) &&
               Instagram.location_recent_media(location_id)
   end
 end
